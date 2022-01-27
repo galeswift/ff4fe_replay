6a9e9e52f26d4bfca30f50afc296d7c1f29abf9b
Keith <kmiron@gmail.com>
2022-01-26 23:00:40 -0800

    Starting ff4fe_replay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import ffmap_overworld from './images/ff4map_overworld.png';
+import ffmap_underworld from './images/ff4map_underworld.png';
+import ffmap_moon from './images/ff4map_moon.png';
+import './styles/ff4fe_replay.css';
+import { MapInteractionCSS } from 'react-map-interaction';
+import { useEffect,useState } from 'react';
 
-function App() {
+function Main()
+{
+  const [mapIndex, setMapIndex] = useState(0);
+
+  const fnOnKeyDown = (event) =>
+  {
+    console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);
+    setMapIndex(mapIndex => (mapIndex+1) % 3);
+  };
+
+  useEffect(()=>
+  {
+    document.addEventListener('keydown', fnOnKeyDown);
+
+    return function cleanup(){
+      document.removeEventListener('keydown',fnOnKeyDown);
+    }
+  },[]);
+
+  var mapName = "";
+  switch (mapIndex)
+  {
+    case 0:
+      mapName = ffmap_overworld;
+      break;
+    case 1:
+      mapName = ffmap_underworld;
+      break;
+    case 2:
+      mapName = ffmap_moon;
+      break;
+  }
+
+  return <MapInteractionCSS>
+      <div className="InReplayRoot">
+        <img className="OverworldMap" src={mapName} alt=""></img>
+      </div>
+      </MapInteractionCSS>;
+
+}
+
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
   );
 }
